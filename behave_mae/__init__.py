a2bb5558dbea5574ab60ed4117bfc771451487d8
LucZot <lucas.stoffl@epfl.ch>
2024-12-16 14:46:29 +0100

    pypi_prep

---
+++ behave_mae/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) A. Mathis Group and contributors. All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+from behave_mae.version import __version__
---
+++ behave_mae/version.py
@@ -0,0 +1,6 @@
+# Copyright (c) A. Mathis Group and contributors. All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+__version__ = "0.0.1rc1"
